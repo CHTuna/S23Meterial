6c6f8b0b9a3f2c3b0dc3f35cf2f982708043600c
EIVE <45841115+CHTuna@users.noreply.github.com>
2023-12-13 14:46:09 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20190008/s23meterial/MainActivity.kt
@@ -0,0 +1,29 @@
+package kr.ac.kumoh.s20190008.s23meterial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.s20190008.s23meterial.ui.theme.S23MeterialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    S23MeterialTheme {
+        SongDrawer()
+    }
+}
